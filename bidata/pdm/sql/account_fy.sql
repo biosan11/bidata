7c8bdf22f2f340b8865fd90c3210fc021fd372d3
jsh <1982797395@qq.com>
2019-09-27 16:07:01 +0800

    费用科目调整，更具彭丽姐提供的邮件确认修改

--- bidata/pdm/sql/account_fy.sql
+++ bidata/pdm/sql/account_fy.sql
@@ -389,3 +389,22 @@ update pdm.account_fy a
  where a.db = 'UFDATA_118_2018'
     or a.db = 'UFDATA_123_2018'
 ;
+
+update pdm.account_fy set kemu = '6602专业机构费',code='66020015',code_name='专业机构费',code_lv2='66020015',code_name_lv2 ='专业机构费' where cohr = '甄元' and kemu = '6601材料'; 
+update pdm.account_fy set kemu = '6601会务费',code='660128',code_name='会务费',code_lv2='660128',code_name_lv2 ='会务费' where cohr = '奥博特' and kemu = '6601会务招待'; 
+update pdm.account_fy set kemu = '6601会务费',code='660128',code_name='会务费',code_lv2='660128',code_name_lv2 ='会务费' where cohr = '美博特' and kemu = '6601会务招待'; 
+update pdm.account_fy set kemu = '6601会务费',code='660128',code_name='会务费',code_lv2='660128',code_name_lv2 ='会务费' where cohr = '博圣' and kemu = '6601会务招待'; 
+update pdm.account_fy set kemu = '6601会务费',code='660128',code_name='会务费',code_lv2='660128',code_name_lv2 ='会务费' where cohr = '博圣' and kemu = '6602会务招待'; 
+update pdm.account_fy set kemu = '6601人员成本',code='6601010104',code_name='交通补贴',code_lv2='660101',code_name_lv2 ='人员成本' where cohr = '博圣' and kemu = '6601交通补贴'; 
+update pdm.account_fy set kemu = '6601人员成本',code='6601010104',code_name='交通补贴',code_lv2='660101',code_name_lv2 ='人员成本' where cohr = '美博特' and kemu = '6601交通补贴'; 
+update pdm.account_fy set kemu = '6602内部会议费',code='660204',code_name='内部会议费',code_lv2='660204',code_name_lv2 ='内部会议费' where cohr = '甄元' and kemu = '6602内部会议费'; 
+update pdm.account_fy set kemu = '6602专业机构费',code='66020015',code_name='专业机构费',code_lv2='66020015',code_name_lv2 ='专业机构费' where cohr = '甄元' and kemu = '6602外部服务费'; 
+update pdm.account_fy set kemu = '6403印花税',code='640305',code_name='印花税',code_lv2='640305',code_name_lv2 ='印花税' where cohr = '恩允' and kemu = '6602印花税'; 
+update pdm.account_fy set kemu = '6403印花税',code='640305',code_name='印花税',code_lv2='640305',code_name_lv2 ='印花税' where cohr = '奥博特' and kemu = '6602印花税'; 
+update pdm.account_fy set kemu = '6403印花税',code='640305',code_name='印花税',code_lv2='640305',code_name_lv2 ='印花税' where cohr = '贝安云' and kemu = '6602印花税'; 
+update pdm.account_fy set kemu = '6601会务费',code='660128',code_name='会务费',code_lv2='660128',code_name_lv2 ='会务费' where cohr = '美博特' and kemu = '6601院内沙龙'; 
+update pdm.account_fy set kemu = '6601耗材及配件',code='660118',code_name='耗材及配件',code_lv2='660118',code_name_lv2 ='耗材及配件' where cohr = '甄元' and kemu = '6601运营费用'; 
+
+
+
+
