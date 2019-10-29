f9669da7b732d62bdbbddf94dd46e02b8ebc09fd
jsh <1982797395@qq.com>
2019-10-29 13:22:44 +0800

    脚本：edw/sql/outdepot_order.sql 说明：更新脚本只取19年以后的数据

--- edw/sql/outdepot_order.sql
+++ edw/sql/outdepot_order.sql
@@ -117,7 +117,8 @@ select a.db
  where (left(a.dnmaketime,10) >= '${start1_dt}' or left(a.dnmodifytime,10) >= '${start1_dt}')
    and a.db <> 'UFDATA_889_2019'
    and a.db <> 'UFDATA_666_2018'
-   and a.db <> 'UFDATA_555_2018'; 
+   and a.db <> 'UFDATA_555_2018'
+   and year(a.ddate) >= '2019'; 
 -- 针对美博特客户不在同部博圣做出调整
 --   and a.ccuscode not in ("001","002","003","004","005","006","007","008","009","010","011","012","013");
 
@@ -156,7 +157,8 @@ select a.db
     on a.ccuscode = b.ccuscode
    and left(a.db,10) = left(b.db,10)
  where (left(a.dnmaketime,10) >= '${start1_dt}' or left(a.dnmodifytime,10) >= '${start1_dt}')
-   and (a.db = 'UFDATA_889_2019' or a.db = 'UFDATA_555_2018' or a.db = 'UFDATA_666_2018');
+   and (a.db = 'UFDATA_889_2019' or a.db = 'UFDATA_555_2018' or a.db = 'UFDATA_666_2018')
+   and year(a.ddate) >= '2019';
 --   and a.ccuscode in ("001","002","003","004","005","006","007","008","009","010","011","012","013");
 
 
