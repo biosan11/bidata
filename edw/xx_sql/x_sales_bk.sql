968833aa4d22c12b76b0023e2806c5476f208e98
biosan-wt <wangtao@biosan.cn>
2019-12-23 13:58:23 +0800

    增加杰毅麦特线下收入

--- edw/xx_sql/x_sales_bk.sql
+++ edw/xx_sql/x_sales_bk.sql
@@ -55,6 +55,34 @@ select a.auto_id
     on a.product_ori = c.cinvname
  where db = 'ZYSY' or db = 'ZYJK'
 ;
+insert into edw.x_sales_bk_pre
+select a.auto_id
+      ,a.db
+      ,a.ddate
+      ,a.product_ori
+      ,a.product_pre
+      ,case when c.cinvname is null then '请核查' 
+            when c.bi_cinvcode is null then '请核查' else c.bi_cinvcode end as bi_cinvcode
+      ,case when c.cinvname is null then '请核查' 
+            when c.bi_cinvname is null then '请核查' else c.cinvname end as bi_cinvname
+      ,a.business_class
+      ,a.iquantity
+      ,a.price
+      ,a.itax
+      ,a.isum
+      ,a.company_invoice
+      ,a.ccusname_ori
+      ,case when b.ccusname is not null then b.bi_cuscode else '请核查' end as bi_cuscode
+      ,case when b.ccusname is not null then b.bi_cusname else '请核查' end as bi_cusname
+  from ufdata.x_sales_bk a
+  left join (select ccusname,bi_cuscode,bi_cusname from edw.dic_customer group by ccusname) b
+    on a.ccusname_ori = b.ccusname
+  left join (select cinvname,bi_cinvcode,bi_cinvname from edw.dic_inventory where db = 'TEMP' group by cinvname) c
+    on a.product_ori = c.cinvname
+ where db = 'JYMT' 
+;
+
+
 
 
 
