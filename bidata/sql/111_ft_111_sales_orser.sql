54708079b839b18538ed39a92eeee4da4e04b9fb
biosan-wt <wangtao@biosan.cn>
2020-01-17 17:49:17 +0800

    更新

--- bidata/sql/111_ft_111_sales_orser.sql
+++ bidata/sql/111_ft_111_sales_orser.sql
@@ -1,14 +1,15 @@
 /*
 CREATE TABLE `ft_111_sales_order` (
-	`csocode` varchar(30) DEFAULT NULL COMMENT '销售订单号',
+  `csocode` varchar(30) DEFAULT NULL COMMENT '销售订单号',
+  `ddate` datetime DEFAULT NULL COMMENT '单据日期',
   `cohr` varchar(20) DEFAULT NULL COMMENT '公司简称',
   `ccuscode` varchar(20) DEFAULT NULL COMMENT '客户编码',
   `finnal_ccuscode` varchar(20) DEFAULT NULL COMMENT '最终客户正确编码',
   `cbustype` varchar(60) DEFAULT NULL COMMENT '业务类型',
   `cinvcode` varchar(60) DEFAULT NULL COMMENT '存货编码',
   `item_code` varchar(60) DEFAULT NULL COMMENT '项目编码',
-  `itaxunitprice` decimal(30,10) DEFAULT NULL COMMENT '原币含税单价',
-  `iquantity` decimal(30,10) DEFAULT NULL COMMENT '数量',
+  `itaxunitprice` decimal(30,4) DEFAULT NULL COMMENT '原币含税单价',
+  `iquantity` decimal(30,4) DEFAULT NULL COMMENT '数量',
   `itax` decimal(19,4) DEFAULT NULL COMMENT '原币税额',
   `isum` decimal(19,4) DEFAULT NULL COMMENT '原币价税合计',
   KEY `index_ft_111_sales_order_csocode` (`csocode`) USING BTREE
@@ -18,21 +19,15 @@ truncate table bidata.ft_111_sales_order;
 insert into bidata.ft_111_sales_order 
 select
   `csocode` ,
+	ddate,
   `cohr`,
   `ccuscode`,
   `finnal_ccuscode` ,
   `cbustype`,
   `cinvcode`,
   `item_code`,
-<<<<<<< HEAD
   round((ifnull(itaxunitprice,0)/1000),3),
   `iquantity`,
   round((ifnull(itax,0)/1000),3),
   round((ifnull(isum,0)/1000),3)
-=======
-  `itaxunitprice`,
-  `iquantity`,
-  `itax`,
-  `isum`
->>>>>>> 190ec6795aca1fb3be4b5a8a1f6c5610412a232a
 from pdm.sales_order;
