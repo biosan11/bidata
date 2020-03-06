639768d761577efdd77a04830e436a97c499804d
jsh <1982797395@qq.com>
2020-03-06 15:34:40 +0800

    增加产品主辅关系清洗表

---
+++ edw/xx_sql/x_cinv_relation.sql
@@ -0,0 +1,21 @@
+-- 产品主辅关系表
+truncate table edw.x_cinv_relation;
+insert into edw.x_cinv_relation
+select a.source
+      ,b.bi_cinvcode as cinvcode_child
+      ,b.bi_cinvname as cinvname_child
+      ,a.match
+      ,a.mixture_ratio
+      ,c.bi_cinvcode as cinvcode_main
+      ,c.bi_cinvname as cinvname_main
+      ,d.item_code
+      ,d.level_three as item_name
+  from ufdata.x_cinv_relation a
+  left join (select * from edw.dic_inventory group by cinvcode) b
+    on a.cinvcode_child = b.cinvcode
+  left join (select * from edw.dic_inventory group by cinvcode) c
+    on a.cinvcode_main = c.cinvcode
+  left join (select * from edw.map_inventory group by bi_cinvcode) d
+    on c.bi_cinvcode = d.bi_cinvcode
+;
+
