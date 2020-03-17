d8845d995badd8428f3eae2164c78ad02fe64ad4
jsh <1982797395@qq.com>
2020-03-17 13:49:46 +0800

    更新地市客单价sql脚本，更新人口来源数据

--- report/sql/ccus_02_market_price.sql
+++ report/sql/ccus_02_market_price.sql
@@ -3,7 +3,7 @@
 ------------------------------------------------------------------------------------------
 --程序名称：ccus_02_market_price.sql
 --目标模型：ccus_02_market_price
---源    表：pdm.invoice_order,report.map_population
+--源    表：pdm.invoice_order,report.ccus_01_map_population
 -----------------------------------------------------------------------------------------
 --加载周期：日增
 ------------------------------------------------------------------------------------------
@@ -64,20 +64,20 @@ select city_get,county,item_code,ddate,inum_person,province_get
 
 
 -- 市级计算，市级人口增加
-drop table if exists report.map_population_pre;
-create temporary table report.map_population_pre as
+drop table if exists report.ccus_01_map_population_pre;
+create temporary table report.ccus_01_map_population_pre as
 select a.city_get
       ,left(a.ddate,7) as y_mon
       ,a.item_code
       ,b.fin_data,a.province_get as province
  from report.x_ccus_delivery_sj a
- left join report.map_population b
+ left join report.ccus_01_map_population b
    on a.city_give = b.city
   and left(a.ddate,7) = left(b.ddate,7)
 ;
 
 -- 区县人口增加
-insert into report.map_population_pre
+insert into report.ccus_01_map_population_pre
 select a.city_get
       ,left(a.ddate,7) as y_mon
       ,a.item_code
@@ -91,43 +91,43 @@ select a.city_get
 ;
 
 -- 收样地市自己存在人口
-insert into report.map_population_pre
+insert into report.ccus_01_map_population_pre
 select a.city_get
       ,left(a.ddate,7) as y_mon
       ,a.item_code
       ,b.fin_data,a.province_get as province
  from (select * from edw.x_ccus_delivery where ddate >= '2019-01-01' group by city_get,ddate,item_code) a
- left join report.map_population b
+ left join report.ccus_01_map_population b
    on a.city_get = b.city
   and left(a.ddate,7) = left(b.ddate,7)
 ;
 
-drop table if exists report.map_population_shou;
-create temporary table report.map_population_shou as
+drop table if exists report.ccus_01_map_population_shou;
+create temporary table report.ccus_01_map_population_shou as
 select a.city_get as city
       ,a.y_mon
       ,a.item_code
       ,sum(a.fin_data) as fin_data,province
-  from report.map_population_pre a
+  from report.ccus_01_map_population_pre a
  group by city_get,y_mon,item_code
 ;
 
 
 -- 送样区县处理
-drop table if exists report.map_population_pre1;
-create temporary table report.map_population_pre1 as
+drop table if exists report.ccus_01_map_population_pre1;
+create temporary table report.ccus_01_map_population_pre1 as
 select a.city_give
       ,left(a.ddate,7) as y_mon
       ,a.item_code
       ,b.fin_data,a.province_get as province
  from (select * from edw.x_ccus_delivery where ddate >= '2019-01-01' and city_grade = '县级' group by city_give,ddate,item_code) a
- left join report.map_population b
+ left join report.ccus_01_map_population b
    on a.city_give = b.city
   and left(a.ddate,7) = left(b.ddate,7)
 ;
 
 -- 插入区县送出去的情况
-insert into report.map_population_pre1
+insert into report.ccus_01_map_population_pre1
 select a.city_give
       ,left(a.ddate,7) as y_mon
       ,a.item_code
@@ -140,26 +140,26 @@ select a.city_give
    and b.inum_person <> 0
 ;
 
-drop table if exists report.map_population_song;
-create temporary table report.map_population_song as
+drop table if exists report.ccus_01_map_population_song;
+create temporary table report.ccus_01_map_population_song as
 select a.city_give as city
       ,a.y_mon
       ,a.item_code,province
       ,sum(a.fin_data) as fin_data
-  from report.map_population_pre1 a
+  from report.ccus_01_map_population_pre1 a
  group by city_give,y_mon,item_code
 ;
 
 
 
 -- 除去收样地市其余的地市情况
-drop table if exists report.map_population_else;
-create temporary table report.map_population_else as
+drop table if exists report.ccus_01_map_population_else;
+create temporary table report.ccus_01_map_population_else as
 select a.city
       ,left(ddate,7) as y_mon
       ,a.fin_data
-  from report.map_population a
-  left join report.map_population_shou b
+  from report.ccus_01_map_population a
+  left join report.ccus_01_map_population_shou b
     on a.city = b.city
  where b.city is null
 ;
@@ -180,7 +180,7 @@ select a.province
       ,case when isum = 0 or fin_data = 0 then 0 else a.isum / b.fin_data end as pct
       ,'无关系'
   from report.invoice_order_pre a
-  left join report.map_population_else b
+  left join report.ccus_01_map_population_else b
     on a.city = b.city
    and a.y_mon = b.y_mon
  where b.city is not null
@@ -199,7 +199,7 @@ select a.province
       ,case when isum = 0 or fin_data = 0 then 0 else a.isum / b.fin_data end as pct
       ,'收样'
   from report.invoice_order_pre a
-  left join report.map_population_shou b
+  left join report.ccus_01_map_population_shou b
     on a.city = b.city
    and a.y_mon = b.y_mon
    and a.item_code = b.item_code
@@ -219,7 +219,7 @@ select a.province
       ,case when isum = 0 or fin_data = 0 then 0 else a.isum / b.fin_data end as pct
       ,'送样'
   from report.invoice_order_pre a
-  left join report.map_population_song b
+  left join report.ccus_01_map_population_song b
     on a.city = b.city
    and a.y_mon = b.y_mon
    and a.item_code = b.item_code
@@ -250,7 +250,7 @@ select a.province
       ,case when isum = 0 or fin_data = 0 then 0 else a.isum / b.fin_data end as pct
       ,'无关系'
   from report.invoice_order_pre1 a
-  left join report.map_population b
+  left join report.ccus_01_map_population b
     on a.city = b.city
    and a.y_mon = left(ddate,7)
  where b.city is not null
@@ -267,7 +267,7 @@ select a.province
       ,b.item_name
       ,a.y_mon
       ,a.fin_data
-  from report.map_population_shou a
+  from report.ccus_01_map_population_shou a
   left join (select * from report.ccus_02_market_price group by city,cinvcode) b
     on a.city = b.city
    and a.item_code = b.item_code
@@ -283,7 +283,7 @@ select a.province
       ,b.item_name
       ,a.y_mon
       ,a.fin_data
-  from report.map_population_song a
+  from report.ccus_01_map_population_song a
   left join (select * from report.ccus_02_market_price group by city,cinvcode) b
     on a.city = b.city
    and a.item_code = b.item_code
@@ -326,7 +326,7 @@ select a.province
       ,b.item_name
       ,left(ddate,7) as y_mon
       ,a.fin_data
-  from report.map_population a
+  from report.ccus_01_map_population a
   left join (select * from report.ccus_02_market_price group by city,cinvcode) b
     on a.city = b.city
  where year(ddate) >= '2019'
