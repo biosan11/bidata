5358292291dd75133aa222dd4d492f7160ccf1d0
jsh <1982797395@qq.com>
2020-03-19 16:11:16 +0800

    合同数据增加产品的清洗

--- edw/xx_sql/x_insure_cover.sql
+++ edw/xx_sql/x_insure_cover.sql
@@ -6,6 +6,8 @@ select a.province
       ,case when a.hospital is not null and b.ccusname is null then '请核查' else b.bi_cuscode end as bi_cuscode
       ,case when a.hospital is not null and b.ccusname is null then '请核查' else b.bi_cusname end as bi_cusname
       ,a.item_name
+      ,case when a.item_name is not null and c.bi_cinvcode is null then '请核查' else c.bi_cinvcode end as bi_cinvcode
+      ,case when a.item_name is not null and c.bi_cinvname is null then '请核查' else c.bi_cinvname end as bi_cinvname
       ,a.ddate
       ,a.insure_num
       ,a.tickling
@@ -17,4 +19,6 @@ select a.province
   from ufdata.x_insure_cover a
   left join (select * from edw.dic_customer group by ccusname) b
     on a.hospital = b.ccusname
+  left join (select * from edw.dic_inventory group by cinvname) c
+    on a.item_name = c.cinvname
 ;
