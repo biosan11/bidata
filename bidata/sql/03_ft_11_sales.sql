8f7bde567c0caa87e6a80469746c279b0ecbb0c1
biosan-wt <55875191+biosan-wt@users.noreply.github.com>
2020-04-02 14:55:18 +0800

    Update 03_ft_11_sales.sql
    增加客户名称、最终客户名称字段

--- bidata/sql/03_ft_11_sales.sql
+++ bidata/sql/03_ft_11_sales.sql
@@ -1,30 +1,32 @@
 -- 2_ft_11_sales
 /* test
 -- 建表 bidata.ft_11_sales
-use bidata;
-drop table if exists ft_11_sales;
-create table `ft_11_sales`(
-    `sbvid` int(11) default null comment '销售发票主表标识',
-    `ddate` datetime default null comment '单据日期',
-    `cohr` varchar(20) default null comment '公司简称',
-    `cwhcode` varchar(10) default null comment '仓库编码',
-    `cdepcode` varchar(12) default null comment '部门编码',
-    `ccuscode` varchar(20) default null comment '客户编码',
-    `finnal_ccuscode` varchar(20) default null comment '最终客户正确编码',
-    `cbustype` varchar(60) default null comment '业务类型',
-    `cinvcode` varchar(60) default null comment '存货编码',
-    `item_code` varchar(60) default null comment '项目编码',
-    `plan_type` varchar(255) default null comment '计划类型:占点、保点、上量、增项',
-    `key_points` varchar(20) default null comment '是否重点',
-    `itaxunitprice` float(13,3) default null comment '原币含税单价',
-    `itax`  float(13,3)  default  null  comment  '原币税额',  
-    `isum`  float(13,3)  default  null  comment  '原币价税合计',   
-    `sys_time` datetime default null comment '数据时间戳',
-    key `index_bidata_bisales_sbvid` (`sbvid`),
-    key `index_bidata_bisales_ccuscode` (`ccuscode`),
-    key `index_bidata_bisales_finnal_ccuscode` (finnal_ccuscode),
-    key `index_bidata_bisales_cinvcode` (cinvcode)
-) engine=innodb default charset=utf8 comment='bi销售收入表';
+CREATE TABLE `ft_11_sales` (
+  `sbvid` int(11) DEFAULT NULL COMMENT '销售发票主表标识',
+  `ddate` datetime DEFAULT NULL COMMENT '单据日期',
+  `cohr` varchar(20) DEFAULT NULL COMMENT '公司简称',
+  `cwhcode` varchar(10) DEFAULT NULL COMMENT '仓库编码',
+  `cdepcode` varchar(12) DEFAULT NULL COMMENT '部门编码',
+  `ccuscode` varchar(20) DEFAULT NULL COMMENT '客户编码',
+  `ccusname` varchar(100) DEFAULT NULL COMMENT '客户名称',
+  `finnal_ccuscode` varchar(20) DEFAULT NULL COMMENT '最终客户正确编码',
+  `finnal_ccusname` varchar(100) DEFAULT NULL COMMENT '最终客户名称',
+  `cbustype` varchar(60) DEFAULT NULL COMMENT '业务类型',
+  `cinvcode` varchar(60) DEFAULT NULL COMMENT '存货编码',
+  `item_code` varchar(60) DEFAULT NULL COMMENT '项目编码',
+  `plan_type` varchar(255) DEFAULT NULL COMMENT '计划类型:占点、保点、上量、增项',
+  `key_points` varchar(20) DEFAULT NULL COMMENT '是否重点',
+  `itaxunitprice` float(13,3) DEFAULT NULL COMMENT '原币含税单价',
+  `itax` float(13,3) DEFAULT NULL COMMENT '原币税额',
+  `isum` float(13,3) DEFAULT NULL COMMENT '原币价税合计',
+  `areadirector` varchar(60) DEFAULT NULL,
+  `cverifier` varchar(60) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `sys_time` datetime DEFAULT NULL COMMENT '数据时间戳',
+  KEY `index_bidata_bisales_sbvid` (`sbvid`),
+  KEY `index_bidata_bisales_ccuscode` (`ccuscode`),
+  KEY `index_bidata_bisales_finnal_ccuscode` (`finnal_ccuscode`),
+  KEY `index_bidata_bisales_cinvcode` (`cinvcode`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='bi销售收入表';
  */
  
 -- bidata.ft_11_sales取部分字段
@@ -37,7 +39,9 @@ select
 	,cwhcode
 	,cdepcode
 	,ifnull(ccuscode,"unknowncus")
+	,ifnull(ccusname,"unknowncus")
 	,finnal_ccuscode
+	,finnal_ccusname
 	,ifnull(cbustype,"产品类")
 	,cinvcode
 	,ifnull(item_code,"其他")
