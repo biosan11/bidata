c4fd83e1f498a9d315571cadd0cea2888747f5f6
biosan-jin <55874892+biosan-jin@users.noreply.github.com>
2020-06-08 18:10:45 +0800

    Update kpi_02_newcus_base_person.sql
    增加计划中的预期完成率字段, 详见需求表

--- report/sql/kpi_02_newcus_base_person.sql
+++ report/sql/kpi_02_newcus_base_person.sql
@@ -14,37 +14,36 @@
 -- 版本控制：版本号  提交人   提交日期   提交内容
 -- 
 -- 调用方法　
---  建表脚本
--- use report;
--- drop table if exists report.kpi_02_newcus_base_person;
--- create table if not exists report.kpi_02_newcus_base_person(
---     mark_1 varchar(20) comment '实际or计划标记',
---     mark_2 varchar(20) comment '新客户or新项目',
---     ddate date comment '日期',
---     cohr varchar(30) comment '公司',
---     ccuscode varchar(30) comment '客户编码',
---     ccusname varchar(255) comment '客户名称',
---     screen_class varchar(30) comment '筛诊分类',
---     sales_dept varchar(30) comment '销售部门',
---     sales_region_new varchar(30) comment '销售区域',
---     province varchar(60) comment '省份',
---     new_item varchar(60) comment '定义新项目', 
---     cverifier_ori varchar(50) comment '销售负责人_未处理',
---     areadirector_ori varchar(50) comment '区域主管_未处理',
---     cverifier_result varchar(50) comment '销售负责人_处理后',
---     areadirector_result varchar(50) comment '区域主管_处理后',
--- key repot_kpi_02_newcus_person_cohr (cohr),
--- key repot_kpi_02_newcus_person_ccuscode (ccuscode)
--- )engine=innodb default charset=utf8 comment='2020年新客户新项目开发基础数据(到人)';
-
+/* 建表脚本
+use report;
+drop table if exists report.kpi_02_newcus_base_person;
+create table if not exists report.kpi_02_newcus_base_person(
+    mark_1 varchar(20) comment '实际or计划标记',
+    mark_2 varchar(20) comment '新客户or新项目',
+    ddate date comment '日期',
+    cohr varchar(30) comment '公司',
+    ccuscode varchar(30) comment '客户编码',
+    ccusname varchar(255) comment '客户名称',
+    screen_class varchar(30) comment '筛诊分类',
+    sales_dept varchar(30) comment '销售部门',
+    sales_region_new varchar(30) comment '销售区域',
+    province varchar(60) comment '省份',
+    new_item varchar(60) comment '定义新项目', 
+    plan_success_rate decimal(5,2) comment '最高成功率',
+    cverifier_ori varchar(50) comment '销售负责人_未处理',
+    areadirector_ori varchar(50) comment '区域主管_未处理',
+    cverifier_result varchar(50) comment '销售负责人_处理后',
+    areadirector_result varchar(50) comment '区域主管_处理后',
+key repot_kpi_02_newcus_person_cohr (cohr),
+key repot_kpi_02_newcus_person_ccuscode (ccuscode)
+)engine=innodb default charset=utf8 comment='2020年新客户新项目开发基础数据(到人)';
+*/
 
 -- 对来源数据按客户 日期升序
 drop temporary table if exists report.newcus_tem00;
 create temporary table if not exists report.newcus_tem00 
 select * from pdm.cusitem_person_newstate 
 order by cuscode,ddate asc;
-
-
 
 
 -- 新客户  不取杭州贝生 
@@ -62,6 +61,7 @@ select
     ,b.sales_region_new
     ,b.province
     ,null as new_item
+    ,max(a.plan_success_rate) as plan_success_rate
     ,a.cverifier as cverifier_ori
     ,a.areadirector as areadirector_ori
     ,if(a.cverifier = a.areadirector,null,a.cverifier) as cverifier_result
@@ -88,6 +88,7 @@ select
     ,b.sales_region_new
     ,b.province
     ,null as new_item
+    ,1 as plan_success_rate
     ,a.cverifier
     ,a.areadirector
     ,if(a.cverifier = a.areadirector,null,a.cverifier) as cverifier_result
@@ -116,6 +117,7 @@ select
     ,b.sales_region_new
     ,b.province
     ,a.new_item
+    ,max(a.plan_success_rate) as plan_success_rate
     ,a.cverifier as cverifier_ori
     ,a.areadirector as areadirector_ori
     ,if(a.cverifier = a.areadirector,null,a.cverifier) as cverifier_result
@@ -142,6 +144,7 @@ select
     ,b.sales_region_new
     ,b.province
     ,a.new_item
+    ,1 as plan_success_rate
     ,a.cverifier as cverifier_ori
     ,a.areadirector as areadirector_ori
     ,if(a.cverifier = a.areadirector,null,a.cverifier) as cverifier_result
@@ -155,4 +158,3 @@ where cohr != "杭州贝生" and status = "老客户" and a.type = "正常开票
 group by a.cuscode,c.screen_class,a.new_item,a.cverifier ,a.areadirector
 order by a.cuscode,a.ddate asc
 ;
-
