b00e53d99de21f53d61e124e5ca2a722f404a6e6
biosan-jin <55874892+biosan-jin@users.noreply.github.com>
2020-06-10 09:16:52 +0800

    Update ehr_employee_information.sql

--- edw/sql/ehr_employee_information.sql
+++ edw/sql/ehr_employee_information.sql
@@ -418,7 +418,7 @@ select a.userid
 -- 实验员部门清洗
 update edw.ehr_employee as a
 join (select * from edw.dic_deptment where source = 'ehr_sy' group by cdept_id) as b 
-on concat(a.cdept_id, a.position_name) = b.cdept_id 
+on concat(a.dept_id, a.position_name) = b.cdept_id 
 set a.cdept_id = b.cdept_id_ehr
 ,a.cdept_name = b.name_ehr
 ,a.cdept_lv2 = b.second_dept
