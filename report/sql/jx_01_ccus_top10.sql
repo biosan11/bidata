6a23498cfd875f7e6422ed37ee20fa373c7e96ec
jsh <1982797395@qq.com>
2020-07-08 16:01:38 +0800

    客户排名当年按照YTD方式来处理，历史不在当前全按照整年统计

--- report/sql/jx_01_ccus_top10.sql
+++ report/sql/jx_01_ccus_top10.sql
@@ -1,19 +1,19 @@
-----------------------------------程序头部----------------------------------------------
---功能：绩效考和对应年度客户top10
-------------------------------------------------------------------------------------------
---程序名称：jx_01_ccus_top10.sql
---目标模型：jx_01_ccus_top10
---源    表：report.fin_11_sales_cost_base
------------------------------------------------------------------------------------------
---加载周期：日增
-------------------------------------------------------------------------------------------
- --作者：jiangsh
- --开发日期：2020-03-09
- ------------------------------------------------------------------------------------------
- --版本控制：版本号  提交人   提交日期   提交内容
- --         V1.0     jiangsh  2020-03-11   开发上线
- --调用方法　sh /home/bidata/report/python/jx_01_ccus_top10.py
- ------------------------------------开始处理逻辑------------------------------------------
+-- --------------------------------程序头部----------------------------------------------
+-- 功能：绩效考和对应年度客户top10
+-- ----------------------------------------------------------------------------------------
+-- 程序名称：jx_01_ccus_top10.sql
+-- 目标模型：jx_01_ccus_top10
+-- 源    表：report.fin_11_sales_cost_base
+-- ---------------------------------------------------------------------------------------
+-- 加载周期：日增
+-- ----------------------------------------------------------------------------------------
+-- 作者：jiangsh
+-- 开发日期：2020-03-09
+-- ----------------------------------------------------------------------------------------
+-- 版本控制：版本号  提交人   提交日期   提交内容
+--          V1.0     jiangsh  2020-03-11   开发上线
+-- 调用方法　sh /home/bidata/report/python/jx_01_ccus_top10.py
+-- ----------------------------------开始处理逻辑------------------------------------------
 
 -- 获取截至指定季度的之前的数据，之前年份数据汇总
 -- 历史数据汇总到年
@@ -24,27 +24,78 @@ select year(ddate) as year_
       ,ccuscode
       ,ccusname
       ,sum(isum) as isum
-  from report.fin_11_sales_cost_base a
- where year(ddate) < year('2020-03-01')
+  from report.fin_11_sales_cost_cw_base a
+ where year(ddate) < year(now()) -- 这里是小于当年数据全部汇总到年
+ and cohr != '杭州贝生' and cohr != '美博特'
+ and type = '正常'  -- 取非关联公司, 非美博特
  group by year_,ccuscode
  order by year_,isum desc
 ;
 
 -- 插入当前年份的每季度的情况
+-- 插入1季度情况
 insert into report.mid1_jx_01_ccus_top10
 select year(ddate) as year_
-      ,case when left(month(ddate) / 3,3) <= 1 then 3
-            when left(month(ddate) / 3,3) <= 2 then 6
-            when left(month(ddate) / 3,3) <= 3 then 9
-            when left(month(ddate) / 3,3) <= 4 then 12
-       end as month_
+      ,3
       ,ccuscode
       ,ccusname
       ,sum(isum) as isum
-  from report.fin_11_sales_cost_base a
- where year(ddate) = year('2020-03-01')
- group by month_,ccuscode
- order by month_,isum desc
+  from report.fin_11_sales_cost_cw_base a
+ where year(ddate) =  year(now())
+   and month(ddate) <= 3
+ and cohr != '杭州贝生' and cohr != '美博特'
+ and type = '正常'  -- 取非关联公司, 非美博特
+ group by ccuscode
+ order by isum desc
+;
+
+
+-- 插入2季度情况
+insert into report.mid1_jx_01_ccus_top10
+select year(ddate) as year_
+      ,6
+      ,ccuscode
+      ,ccusname
+      ,sum(isum) as isum
+  from report.fin_11_sales_cost_cw_base a
+ where year(ddate) =  year(now())
+   and month(ddate) <= 6
+ and cohr != '杭州贝生' and cohr != '美博特'
+ and type = '正常'  -- 取非关联公司, 非美博特
+ group by ccuscode
+ order by isum desc
+;
+
+-- 插入3季度情况
+insert into report.mid1_jx_01_ccus_top10
+select year(ddate) as year_
+      ,9
+      ,ccuscode
+      ,ccusname
+      ,sum(isum) as isum
+  from report.fin_11_sales_cost_cw_base a
+ where year(ddate) =  year(now())
+   and month(ddate) <= 9
+ and cohr != '杭州贝生' and cohr != '美博特'
+ and type = '正常'  -- 取非关联公司, 非美博特
+ group by ccuscode
+ order by isum desc
+;
+
+-- 插入4季度情况
+insert into report.mid1_jx_01_ccus_top10
+select year(ddate) as year_
+      ,12
+      ,ccuscode
+      ,ccusname
+      ,sum(isum) as isum
+  from report.fin_11_sales_cost_cw_base a
+ where year(ddate) =  year(now())
+   and month(ddate) <= 12
+ and cohr != '杭州贝生' and cohr != '美博特'
+ and type = '正常'  -- 取非关联公司, 非美博特
+ group by ccuscode
+ order by isum desc
 ;
 
 -- 当前状态求和，获取一个总收入和总有效客户数
@@ -63,7 +114,7 @@ truncate table report.jx_01_ccus_top10;
 insert into report.jx_01_ccus_top10
 select @r:= case when @year_=a.year_ and @month_ = a.month_ then @r+1 else 1 end as rownum
       ,@year_:=a.year_ as year_
-      ,@month_:=a.month_ as month_
+      ,@month_:=a.month_ as quarter
       ,ccuscode
       ,ccusname
       ,isum
