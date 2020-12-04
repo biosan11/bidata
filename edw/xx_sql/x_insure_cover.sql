fcd04796b1728618b56eee4affa4563ebdaf1929
biosan-jin <55874892+biosan-jin@users.noreply.github.com>
2020-12-04 17:56:48 +0800

    Update x_insure_cover.sql

--- edw/xx_sql/x_insure_cover.sql
+++ edw/xx_sql/x_insure_cover.sql
@@ -6,6 +6,14 @@ select d.province
       ,case when a.hospital is not null and b.ccusname is null then '请核查' else b.bi_cuscode end as bi_cuscode
       ,case when a.hospital is not null and b.ccusname is null then '请核查' else b.bi_cusname end as bi_cusname
       ,a.item_name
+      ,case 
+            when left(a.item_name,2) ='串联' then '串联'
+            when a.item_name = 'NIPT' then 'NIPT'
+            when a.item_name = 'NIPT保险' then 'NIPT'
+            when a.item_name = 'NIPT plus' then 'NIPT-pro'
+            when a.item_name = 'NIPT plus保险' then 'NIPT-pro'
+            else '请核查'
+       end as item_name_ad
       ,null as bi_cinvcode
       ,null as bi_cinvname
       -- ,case when a.item_name is not null and c.bi_cinvcode is null then '请核查' else c.bi_cinvcode end as bi_cinvcode
